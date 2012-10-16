92bb42400645d592d21c971e7681853dbe601491
dinujohn <dinujohn27@gmail.com>
2012-10-15 17:13:09 -0700

    Initial Packages

---
+++ WDMS/src/com/asu/wdms/dao/User.java
@@ -0,0 +1,65 @@
+package com.asu.wdms.dao;
+
+public class User {
+	private int userId;
+	private String emailId;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private int roleId;
+	private int deptId;
+	private int validUser;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public int getValidUser() {
+		return validUser;
+	}
+	public void setValidUser(int validUser) {
+		this.validUser = validUser;
+	}
+	
+
+	
+	
+
+}
